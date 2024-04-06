c66ed9e256f969335949341d8e414acf34a94e2f
thomas_aspa <aspa.thomas@gmail.com>
2024-04-06 16:39:13 +0200

    Schéma type

--- schema.sh
+++ schema.sh
@@ -1,4 +1,14 @@
 python3 question.py question1 topType awardPrice distance
 python3 question.py question1 typeOfContract awardPrice distance
+
 python3 question.py question2 department_suppliers awardPrice numberTenders
 python3 question.py question2 department_buyers awardPrice numberTenders
+
+python3 question.py question2_W department_suppliers awardPrice numberTenders
+python3 question.py question2_W department_buyers awardPrice numberTenders
+
+python3 question.py question2_U department_suppliers awardPrice numberTenders
+python3 question.py question2_U department_buyers awardPrice numberTenders
+
+python3 question.py question2_S department_suppliers awardPrice numberTenders
+python3 question.py question2_S department_buyers awardPrice numberTenders
